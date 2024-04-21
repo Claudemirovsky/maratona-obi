3d08c40579d2bd2382b7f14aa98e8ba6b3372afd
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2024-04-20 21:52:14 -0300

    feat(semana01): Add beecrowd #2413 solution

---
+++ semana01/bc2413.c
@@ -0,0 +1,11 @@
+/* beecrowd - 2413 */
+#include <stdio.h>
+
+int main() {
+    unsigned short pessoas;
+    scanf("%hu", &pessoas);
+
+    pessoas *= 4;
+    printf("%hu\n", pessoas);
+    return 0;
+}
