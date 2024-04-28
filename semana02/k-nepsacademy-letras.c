e48cc793f2ffe5143ee0b8c7b041d23dc562e726
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2024-04-28 15:34:37 -0300

    fix(semana02/k-letras): Fix erroneous assumption, get AC (Thnx JBreno)

--- semana02/k-nepsacademy-letras.c
+++ semana02/k-nepsacademy-letras.c
@@ -1,33 +1,33 @@
-// #include <fenv.h> // Necessário pra arredondar pra baixo no printf
 #include <stdio.h>
 
 int main() {
-    char target = getchar(), skip = 0, current, isinfec = 0;
-    double total_words = 0, infected_words = 0, chars = 0;
+    char target = getchar(), skip = 0, current, isinfec = 0, add = 0;
+    double total_words = 0, infected_words = 0;
     getchar();
 
     while ((current = getchar()) != EOF && current != '\n') {
         if (current == ' ') {
-            chars = 0;
             isinfec = 0;
             skip = 0;
+            add = 0;
             continue;
         }
         if (skip)
             continue;
-        chars++;
+        if (!add) {
+            total_words++;
+            add = 1;
+        }
         if (current == target)
             isinfec = 1;
 
-        if (chars == 2)
-            total_words++;
-        if (isinfec && chars >= 2) {
+        if (isinfec) {
             infected_words++;
             skip = 1;
+            add = 0;
         }
     }
 
-    // fesetround(FE_DOWNWARD); // Necessário pra arredondar pra baixo no printf
     if (total_words == 0 || infected_words == 0)
         printf("0.0\n");
     else
