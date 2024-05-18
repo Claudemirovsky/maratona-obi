c855cff4fa2fb0dc3b4b251e46a1e64bfd313c5d
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2024-05-18 19:00:00 -0300

    fix(semana04/e-neps-501-highway): Fix solution with help from J.Breno & C.Cidade

--- semana04/e-neps-501-highway.c
+++ semana04/e-neps-501-highway.c
@@ -1,27 +1,43 @@
 #include <stdio.h>
 #include <string.h>
 
+#define zerofill(arr) memset(arr, 0, (cities + 1) * sizeof(arr[0]))
+
 int main() {
+    // Borrowed tips from J.Breno
     int cities;
     scanf("%d", &cities);
+    char freq[cities + 1];
     int roads[cities + 1];
-    memset(roads, 0, sizeof(roads));
-    char visited[cities + 1];
-    memset(visited, 0, sizeof(visited));
-
+    zerofill(freq);
+    zerofill(roads);
     for (int i = 0; i < cities; i++) {
-        int c1, c2;
-        scanf("%d %d", &c1, &c2);
-        roads[c1] = c2;
+        int next, current;
+        scanf("%d %d", &next, &current);
+        if (freq[current] >= 2 || freq[next] >= 2) {
+            printf("N\n");
+            return 0;
+        }
+        freq[next]++;
+        freq[current]++;
+        roads[next] = current;
     }
 
-    int current = 1, total = 0;
-    for (int i = 0; i < cities && roads[current] && !visited[current]; i++) {
-        visited[current] = 1;
-        total++;
-        current = roads[current];
+    for (int i = 1; i <= cities; i++) {
+        if (freq[i] == 2)
+            continue;
+        printf("N\n");
+        return 0;
     }
 
-    printf("%c\n", (total >= cities) ? 'S' : 'N');
+    int index = 1;
+    while (roads[index]) {
+        int t = roads[index];
+        roads[index] = 0;
+        cities--;
+        index = t;
+    }
+
+    printf("%c\n", cities ? 'N' : 'S');
     return 0;
 }
