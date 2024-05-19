57b643f053b7e922ccacddaf638f5fae5c083e53
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2024-05-19 17:20:58 -0300

    refactor(semana05/g-neps-56-braces): Use getchar instead of getline, get rid of dynamic memory

--- semana05/g-neps-56-braces.c
+++ semana05/g-neps-56-braces.c
@@ -1,28 +1,19 @@
-#include <malloc.h>
 #include <stdio.h>
 
 int main() {
     int lines, count = 0;
     scanf("%d", &lines);
-    lines++;
-    while (lines--) {
-        char *str = NULL;
-        size_t len = 30;
-        getline(&str, &len, stdin);
-        for (int i = 0; i < len; i++) {
-            char ch = str[i];
-            if (ch == '{') {
-                count++;
-            } else if (ch == '}') {
-                if (count <= 0) {
-                    printf("N\n");
-                    free(str);
-                    return 0;
-                }
-                count--;
+    char ch;
+    while ((ch = getchar()) != EOF) {
+        if (ch == '{') {
+            count++;
+        } else if (ch == '}') {
+            if (count <= 0) {
+                printf("N\n");
+                return 0;
             }
+            count--;
         }
-        free(str);
     }
 
     printf("%c\n", count ? 'N' : 'S');
